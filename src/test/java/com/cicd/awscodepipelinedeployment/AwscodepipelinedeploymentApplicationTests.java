e169cdb10e0f5b0d859c59396754b92c4d7f0a52
Fathima Shabnam C B <cbshabnamfathima@gmail.com>
2023-10-04 12:02:29 +0530

    first commit

---
+++ src/test/java/com/cicd/awscodepipelinedeployment/AwscodepipelinedeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.awscodepipelinedeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwscodepipelinedeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
